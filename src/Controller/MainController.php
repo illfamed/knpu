6fb7e5c342c87285da488cf9f9cceb1b5141048b
illfamed <generalarmyhf@gmail.com>
2024-10-23 02:23:17 +0200

    ep12 done

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\StarshipRepository;
+use Symfony\Bridge\Twig\Command\DebugCommand;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
