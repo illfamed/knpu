d1292d7e4ee3553c7c67e513398fb6a8ef92379a
illfamed <generalarmyhf@gmail.com>
2024-10-23 04:08:39 +0200

    ep14 done

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,39 +3,23 @@
 namespace App\Controller;
 
 use App\Repository\StarshipRepository;
-use Symfony\Bridge\Twig\Command\DebugCommand;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\Cache\ItemInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
     public function homepage(
         StarshipRepository $starshipRepository,
-        HttpClientInterface $client,
-        CacheInterface $issLocationPool,
         ): Response
     {
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
 
-        $issData = $issLocationPool->get('iss_location_data', function () use($client): array {
-            $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
-            return $response->toArray();
-    
-        });
-
         return $this->render('main/homepage.html.twig', [
             'myShip' => $myShip,
             'ships' => $ships,
-            'issData' => $issData,
         ]);
     }
 }
